cf673387eb34146b147fbf03d2d3cef862d96b9e
Goutham Peepala <goutham@routethis.com>
2018-11-29 12:27:20 -0500

    Initial Commit to GitHub

---
+++ Tutorials/clusterVisits/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    host : 'redis-server'
+});
+client.set('visits', 0);
+app.get('/', (req, res) => {
+    client.get('visits', (err,visits)=>{
+        res.send('The Number of Visits are '+ visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8088,()=>{
+    console.log("Hey you just started the visitor server");
+});
